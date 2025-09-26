cf219d8eb862bc1270545318a056acb7916c6e05
Intelligent Vehicles and Systems Group (Prof. Sean Brennan) <prof.brennan@gmail.com>
2025-09-26 02:58:04 -0400

    FEAT: Add string replacement and file renaming utilities
    Introduces fcn_DebugTools_replaceStringInDirectory for replacing strings in files within a directory, a script for batch renaming files, and a test script for the string replacement function. These tools assist with bulk updates and refactoring of file names and contents in the Functions directory.

---
+++ Functions/fcn_DebugTools_replaceStringInDirectory.m
@@ -0,0 +1,208 @@
+function fcn_DebugTools_replaceStringInDirectory(directoryPath, oldString, newString, varargin)
+% fcn_DebugTools_replaceStringInDirectory
+% Replaces 'oldString' with 'newString' in all files within 'directoryPath'.
+%
+% FORMAT:
+%
+%      fcn_DebugTools_replaceStringInDirectory(directoryPath, oldString, newString,(fig_num));
+%
+% INPUTS:
+%
+%      directoryPath: a string representing the directory to search.
+%
+%      oldString: the string to replace
+%
+%      newString: the string that will be substituted into oldString
+%
+%      (OPTIONAL INPUTS)
+%
+%      fig_num: a figure number to plot results.
+%
+% OUTPUTS:
+%
+%      (none)
+
+%
+% DEPENDENCIES:
+%
+%      fcn_DebugTools_checkInputsToFunctions
+%
+% EXAMPLES:
+%
+%     See the script: script_test_fcn_DebugTools_replaceStringInDirectory
+%     for a full test suite.
+%
+% This function was written on 2025_09_26 by S. Brennan
+% Questions or comments? sbrennan@psu.edu
+
+% Revision history:
+% 2025_09_26 - sbrennan@psu.edu
+% -- wrote the code originally, using breakDataIntoLaps as starter
+
+% TO-DO
+% (none)
+
+%% Debugging and Input checks
+
+% Check if flag_max_speed set. This occurs if the fig_num variable input
+% argument (varargin) is given a number of -1, which is not a valid figure
+% number.
+MAX_NARGIN = 4; % The largest Number of argument inputs to the function
+flag_max_speed = 0; % The default. This runs code with all error checking
+if (nargin==MAX_NARGIN && isequal(varargin{end},-1))
+    flag_do_debug = 0; % Flag to plot the results for debugging
+    flag_check_inputs = 0; % Flag to perform input checking
+    flag_max_speed = 1;
+else
+    % Check to see if we are externally setting debug mode to be "on"
+    flag_do_debug = 0; % Flag to plot the results for debugging
+    flag_check_inputs = 1; % Flag to perform input checking
+    MATLABFLAG_LAPS_FLAG_CHECK_INPUTS = getenv("MATLABFLAG_LAPS_FLAG_CHECK_INPUTS");
+    MATLABFLAG_LAPS_FLAG_DO_DEBUG = getenv("MATLABFLAG_LAPS_FLAG_DO_DEBUG");
+    if ~isempty(MATLABFLAG_LAPS_FLAG_CHECK_INPUTS) && ~isempty(MATLABFLAG_LAPS_FLAG_DO_DEBUG)
+        flag_do_debug = str2double(MATLABFLAG_LAPS_FLAG_DO_DEBUG);
+        flag_check_inputs  = str2double(MATLABFLAG_LAPS_FLAG_CHECK_INPUTS);
+    end
+end
+
+% flag_do_debug = 1;
+
+if flag_do_debug % If debugging is on, print on entry/exit to the function
+    st = dbstack; %#ok<*UNRCH>
+    fprintf(1,'STARTING function: %s, in file: %s\n',st(1).name,st(1).file);
+    debug_fig_num = 999978; %#ok<NASGU>
+else
+    debug_fig_num = []; %#ok<NASGU>
+end
+
+%% check input arguments?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   _____                   _
+%  |_   _|                 | |
+%    | |  _ __  _ __  _   _| |_ ___
+%    | | | '_ \| '_ \| | | | __/ __|
+%   _| |_| | | | |_) | |_| | |_\__ \
+%  |_____|_| |_| .__/ \__,_|\__|___/
+%              | |
+%              |_|
+% See: http://patorjk.com/software/taag/#p=display&f=Big&t=Inputs
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 0==flag_max_speed
+    if flag_check_inputs
+        % Are there the right number of inputs?
+        narginchk(3,MAX_NARGIN);
+
+        % % Check the input_path to be sure it has 2 or 3 columns, minimum 2 rows
+        % % or more
+        % fcn_DebugTools_checkInputsToFunctions(input_path, '2or3column_of_numbers',[2 3]);
+    end
+end
+
+
+% Does user want to show the plots?
+flag_do_plots = 0; % Default is to NOT show plots
+if (0==flag_max_speed) && (MAX_NARGIN == nargin) 
+    temp = varargin{end};
+    if ~isempty(temp) % Did the user NOT give an empty figure number?
+        fig_num = temp;
+        figure(fig_num);
+        flag_do_plots = 1;
+    end
+end
+
+%% Main code starts here
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   __  __       _
+%  |  \/  |     (_)
+%  | \  / | __ _ _ _ __
+%  | |\/| |/ _` | | '_ \
+%  | |  | | (_| | | | | |
+%  |_|  |_|\__,_|_|_| |_|
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Get a list of all files in the directory
+fileList = dir(fullfile(directoryPath, '*.*')); % Adjust file extension as needed
+
+% Filter out directories from the list
+fileList = fileList(~[fileList.isdir]);
+
+
+for i = 1:length(fileList)
+    fileName = fileList(i).name;
+    if contains(fileName,newString)
+
+        filePath = fullfile(directoryPath, fileName);
+        numChanged = 0;
+        numSkipped = 0;
+        try
+            % Read the entire file content
+            fileContent = readlines(filePath);
+
+            % Perform the string replacement
+            modifiedContent = fileContent;
+            for ith_line = 1:length(fileContent(:,1))
+                thisLine = fileContent(ith_line,1);
+                thisLineCharacters = char(thisLine);
+                if contains(thisLine,oldString)
+                    if thisLineCharacters(1)~='%'
+                        modifiedContent(ith_line,1) = replace(thisLine, oldString, newString);
+                        numChanged = numChanged+1;
+                    else
+                        numSkipped = numSkipped+1;
+                    end
+                end
+            end
+
+            % Write the modified content back to the file
+            writelines(modifiedContent, filePath);
+
+            % For debugging - use the line below to put a breakpoint
+            if numChanged~=0
+                temp = 2; %#ok<NASGU>
+            end
+
+            fprintf('Changed %.0f instances, skipped %.0f comments in %s\n', numChanged, numSkipped, fileName);
+        catch ME
+            warning('Error processing file %s: %s\n', fileName, ME.message);
+        end % Ends try
+    end % Ends check if file contains
+end % Ends for loop
+
+%% Plot the results (for debugging)?
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   _____       _
+%  |  __ \     | |
+%  | |  | | ___| |__  _   _  __ _
+%  | |  | |/ _ \ '_ \| | | |/ _` |
+%  | |__| |  __/ |_) | |_| | (_| |
+%  |_____/ \___|_.__/ \__,_|\__, |
+%                            __/ |
+%                           |___/
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if flag_do_plots
+    
+   % Nothing to plot
+
+    
+end
+
+if flag_do_debug
+    fprintf(1,'ENDING function: %s, in file: %s\n\n',st(1).name,st(1).file);
+end
+
+end % Ends main function
+
+%% Functions follow
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   ______                _   _
+%  |  ____|              | | (_)
+%  | |__ _   _ _ __   ___| |_ _  ___  _ __  ___
+%  |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+%  | |  | |_| | | | | (__| |_| | (_) | | | \__ \
+%  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+%
+% See: https://patorjk.com/software/taag/#p=display&f=Big&t=Functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%§
+
+
---
+++ Functions/script_DebugTools_changeNames.m
@@ -0,0 +1,26 @@
+% Renames all files with one repo name to the name of another
+
+thisPath = fullfile(cd,'Functions');
+directoryList = dir(thisPath);
+
+stringToFind = '_DataClean_';
+stringToReplaceWith = '_LoadRawDataToMATLAB_';
+
+numChanged = 0;
+for ith_file = 1:length(directoryList)
+    thisName = directoryList(ith_file).name;
+    if contains(thisName,stringToFind)
+        newName = replace(thisName,stringToFind,stringToReplaceWith);
+        fullnameSource = fullfile(cd,'Functions',thisName);
+        fullnameDestination = fullfile(cd,'Functions',newName);
+        [moveStatus,moveMessage,moveMessageID] = movefile(fullnameSource,fullnameDestination,'f');
+        if moveStatus~=1
+            fprintf(1,'moveMessage: %s\n',moveMessage);
+            fprintf(1,'moveMessageID: %s\n', moveMessageID);
+            error('Move was not successful?');
+        end
+        fprintf('Changed %s \nto %s\n\n',thisName,newName);
+        numChanged = numChanged+1;
+    end
+end
+fprintf(1,'Number of changed names: %.0f\n',numChanged)
