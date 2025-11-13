bb20b3692e625dc3d774ad3a54d00c12fd56f9c4
Intelligent Vehicles and Systems Group (Prof. Sean Brennan) <prof.brennan@gmail.com>
2025-11-13 15:24:58 -0500

    FEAT: fixed bug with file path extraction in test script
    Moved the extraction of the current file path and root folder path to the beginning of the root folder checks section for improved code clarity and organization.

--- Functions/script_test_all_functions.m
+++ Functions/script_test_all_functions.m
@@ -30,17 +30,18 @@ outputFile = cat(2,'script_test_fcn',repoShortName,'all_stdout.txt');
 diary(fullfile(pwd,outputFile));
 
 %% Root folder checks start here
+
+st = dbstack; 
+thisFile = which(st(1).file);
+[filepath,name,ext] = fileparts(thisFile);
+rootFilePath = extractBefore(filepath,'Functions');
+
 fcn_DebugTools_cprintf('*blue','Checking root folder:\n\t%s:\n', filepath);
 
 
 %% Make sure there is only one .m file in main folder
 fcn_DebugTools_cprintf('*blue','');
 fcn_DebugTools_cprintf('*blue','\tChecking that there is 1 m-file in root folder: ');
-
-st = dbstack; 
-thisFile = which(st(1).file);
-[filepath,name,ext] = fileparts(thisFile);
-rootFilePath = extractBefore(filepath,'Functions');
 
 fileListRootFolder = dir(rootFilePath);
 [...
